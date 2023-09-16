cead0ca5146a324629626864be4062e9dedcaf77
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-16 03:10:29 +0200

    [add] Funzione per verificare pari o dispari

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // Associo event listener al pulsante di verifica per la parola palindroma
 document.getElementById("verificaPalindromaBtn").addEventListener("click", verificaPalindroma);
-// Da ELIMINARE
-console.log(document.getElementById("verificaPalindromaBtn"));
 
 // Associo event listener al pulsante di verifica per Pari o Dispari
 document.getElementById("verificaPariDispariBtn").addEventListener("click", verificaPariDispariBtn);
-
-// Da ELIMINARE
-console.log(document.getElementById("verificaPariDispariBtn"));
 
 // Creo una funzione per verificare se una parola è palindroma
 function verificaPalindroma() {
@@ -35,4 +30,33 @@ function generaNumeroCasuale() {
 // Creo una funzione per verificare se un numero è pari o dispari
 function isPari(numero) {
     return numero % 2 === 0;
+}
+
+// Funzione per verificare pari o dispari e dichiarare il vincitore
+function verificaPariDispariBtn() {
+
+    // Ottengo la scelta dell'utente
+    const sceltaUtente = document.getElementById("sceltaUtente").value;
+    
+    // Ottengo l'elemento HTML in cui verrà mostrato il risultato
+    const numeroUtente = parseInt(document.getElementById("numeroUtente").value, 10);
+
+    // Creo un numero casuale da 1 a 5
+    const numeroComputer = generaNumeroCasuale();
+
+    // Calcolo la somma dei due numeri
+    const somma = numeroUtente + numeroComputer;
+
+    //Ottengo l'elemento HTML in cui verrà mostrato il risultato
+    const risultatoPariDispari = document.getElementById("risultatoPariDispari");
+
+    // Verifico se la somma è pari o dispari
+    const sommaPari = isPari(somma);
+
+    // Mostra il risultato
+    if ((sceltaUtente === "pari" && sommaPari) || (sceltaUtente === "dispari" && !sommaPari)) {
+        risultatoPariDispari.innerHTML = `Hai vinto! La somma (${somma}) è ${sceltaUtente}.`;
+    } else {
+        risultatoPariDispari.innerHTML = `Hai perso! La somma (${somma}) non è ${sceltaUtente}.`;
+    }
 }
