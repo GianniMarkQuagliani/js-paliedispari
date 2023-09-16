f0632e9c5a7aecd883cf88474e48030e7aadbf20
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-16 02:42:06 +0200

    [add] event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+// Associo event listener al pulsante di verifica per la parola palindroma
+document.getElementById("verificaPalindromaBtn").addEventListener("click", verificaPalindromaBtn);
+// Da ELIMINARE
+console.log(document.getElementById("verificaPalindromaBtn"));
+
+// Associo event listener al pulsante di verifica per Pari o Dispari
+document.getElementById("verificaPariDispariBtn").addEventListener("click", verificaPariDispariBtn);
+
+// Da ELIMINARE
+console.log(document.getElementById("verificaPariDispariBtn"));
